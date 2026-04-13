f5baca6d72583e56b4197504f17a302ed05f88d5
Underscore76 <theunderscore76@gmail.com>
2026-04-13 00:42:52 -0500

    global suppression

---
+++ sdv-plugin/GlobalSuppressions.cs
@@ -0,0 +1,12 @@
+// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:FileProcessor.ProcessFile(System.Byte[])~System.Threading.Tasks.Task{System.Int32}")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:SeedFinding.SearchFunctions.CancelFairySeedSearch(System.String)~System.Threading.Tasks.Task{System.String}")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:SeedFinding.SearchFunctions.GetFairySeedSearchStatus(System.String)~System.Threading.Tasks.Task{System.String}")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:SeedFinding.SearchFunctions.GetThreadSettings~System.Threading.Tasks.Task{System.String}")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:SeedFinding.SearchFunctions.StartFairySeedSearch(System.Boolean,System.Int32,System.Int32,System.Int32)~System.Threading.Tasks.Task{System.String}")]
